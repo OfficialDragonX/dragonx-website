4d05cc398f18ce3c372899e53ebd42e50ff8300e
OfficialDragonX <officialdragonx763@gmail.com>
2026-07-29 17:00:02 +0545

    Add website JavaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DragonX (DRGX) Website Loaded Successfully");
+});
+
+function comingSoon() {
+    alert("🚀 More DragonX features are coming soon!");
+}
