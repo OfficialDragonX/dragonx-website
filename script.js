12fc626464130b8b6d583bb8aef9eb04e0db64ed
OfficialDragonX <officialdragonx763@gmail.com>
2026-08-01 17:33:27 +0545

    Implement page load loader functionality
    Add a loader that fades out after the page loads

--- script.js
+++ script.js
@@ -82,3 +82,22 @@ menuToggle.addEventListener("click", () => {
     menuToggle.innerHTML = "☰";
   }
 });
+
+// Loader
+window.addEventListener("load", () => {
+
+const loader = document.getElementById("loader");
+
+setTimeout(() => {
+
+loader.style.opacity = "0";
+
+setTimeout(() => {
+
+loader.style.display = "none";
+
+},800);
+
+},1200);
+
+});
